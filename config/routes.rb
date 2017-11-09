decb19e3d7755e0dea97377b8e8f8cd19553f485
svroomen <vroomen28@outlook.com>
2017-11-08 23:29:33 -0600

    Added menu bar: - Added the menu bar with links - Updated the routes file - Created the customer, home, and products controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'home#index'
+  resources :home, only: :index
+  resources :customers
+  resources :products, only: %i[index show]
 end
