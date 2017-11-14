acbc82265ad78fe515b78852b5a1e69673067e06
svroomen <vroomen28@outlook.com>
2017-11-13 22:46:14 -0600

    Added Product Search: - Products can be search by a keyword that matches anything in the description or name of the product - Product searches can by limited by category - Updated site styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :load_search_categories
+
+  protected
+
+  def load_search_categories
+    # we save the results in a session so that the query only runs once
+    # instead of on every page. It will still update the search categories
+    # session variable whenever the user starts a new session, or when
+    # the current session times out. (default timeout is 30 min)
+    unless session[:search_categories].present?
+      session[:search_categories] =
+        Category.order(:name).map { |c| [c.name, c.id] }
+    end
+  end
 end
